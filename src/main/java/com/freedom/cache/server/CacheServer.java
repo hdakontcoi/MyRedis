9618799bbddb37a06f7077c7895199c833c92e5d
hdakontcoi <ngken7610eoj@126.com>
2021-04-08 22:48:32 +0800

    update files

--- src/main/java/com/freedom/cache/server/CacheServer.java
+++ src/main/java/com/freedom/cache/server/CacheServer.java
@@ -8,7 +8,7 @@ package com.freedom.cache.server;
  */
 import com.freedom.cache.netty.NettyServer;
 
-public class CacheServer {
+public class CacheServer {//基于redis-2.6.14
 	public static void main(String[] args) {
 		// 主函数入口
 		// 1)启动netty
